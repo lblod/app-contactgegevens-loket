6f3255e52be1db3fd2fb4f22fd7c434f4627710b
Oscar Rodriguez Villalobos <lagartoverde97@gmail.com>
2023-09-06 15:05:22 +0200

    added dispatcher routes

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -19,6 +19,50 @@ defmodule Dispatcher do
   # Run `docker-compose restart dispatcher` after updating
   # this file.
 
+  match "/administrative-units/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://resource/administrative-units/"
+  end
+
+  match "/administrative-unit-classification-codes/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/administrative-unit-classification-codes/"
+  end
+
+  match "/organization-status-codes/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/organization-status-codes/"
+  end
+
+  match "/organizations/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/organizations/"
+  end
+
+  match "/identifiers/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/identifiers/"
+  end
+
+  match "/structured-identifiers/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/structured-identifiers/"
+  end
+
+  match "/addresses/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/addresses/"
+  end
+
+  match "/sites/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/sites/"
+  end
+
+  match "/contact-points/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/contact-points/"
+  end
+
+  match "/concepts/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/concepts/"
+  end
+
+  match "/site-types/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/site-types/"
+  end
+
   match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
