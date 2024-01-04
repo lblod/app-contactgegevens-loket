df05d7ce3898c7a168bf5f4b741dcb18f1ce83a6
felix <ruiz.felixruiz@gmail.com>
2024-01-04 14:06:47 +0100

    Update the rules of the delta-notifier to ensure proper syncing

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,6 +1,23 @@
 export default [
     {
       match: {
+        predicate: {
+          type: 'uri',
+          value: 'http://www.w3.org/ns/adms#status'
+        }
+      },
+      callback: {
+        method: 'POST',
+        url: 'http://jobs-controller/delta'
+      },
+      options: {
+        resourceFormat: 'v0.0.1',
+        gracePeriod: 1000,
+        ignoreFromSelf: true
+      }
+    },
+    {
+      match: {
         // anything -> note you can tweak the performance here by filtering on graphs if you need to.
       },
       callback: {
@@ -12,7 +29,27 @@ export default [
         gracePeriod: 1000,
         ignoreFromSelf: true
       }
+    },
+    {
+      match: {
+        predicate: {
+          type: 'uri',
+          value: 'http://www.w3.org/ns/adms#status'
+        },
+        object: {
+          type: 'uri',
+          value: 'http://redpencil.data.gift/id/concept/JobStatus/scheduled'
+        }
+      },
+      callback: {
+        url: 'http://delta-producer-dump-file-publisher/delta',
+        method: 'POST'
+      },
+      options: {
+        resourceFormat: 'v0.0.1',
+        gracePeriod: 1000,
+        ignoreFromSelf: true
+      }
     }
-  
     // Other delta listeners
   ];
