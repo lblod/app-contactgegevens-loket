d07bb823216581859ea90611e1623fd2e4908d2a
Dennis Van Eecke <dennis.van.eecke@codifly.be>
2024-01-25 09:08:20 +0000

    Dashboard now working. But jobs are not being served by resource

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -3,3 +3,5 @@
 ;; reading in the domain.json
 (read-domain-file "domain.json")
 (read-domain-file "change-events.json")
+(read-domain-file "master-job-domain.lisp")
+(read-domain-file "master-reports-domain.lisp")
