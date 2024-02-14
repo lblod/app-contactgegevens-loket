8fdefe81ba0465675e3a241c47c3c7fd2b4b15ee
YJouhar47 <youssef.jouhar1998@hotmail.com>
2024-02-14 17:59:42 +0100

    file service bug fixed

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -2,7 +2,10 @@
 
 ;; reading in the domain.json
 (read-domain-file "domain.json")
+(read-domain-file "file-service.json")
 (read-domain-file "change-events.json")
 (read-domain-file "master-job-domain.lisp")
 (read-domain-file "master-reports-domain.lisp")
 (read-domain-file "master-log-domain.lisp")
+
+
