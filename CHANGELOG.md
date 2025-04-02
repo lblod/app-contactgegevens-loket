cd411df41411119dccefcf7c27b7688bcfe416c0
Claire Lovisa <lovisa.claire@gmail.com>
2025-04-02 20:42:24 +0200

    Refactor consumer and add contact data dispatching

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,16 @@
 ## Unreleased
 - Add missing keys for `migrations`, `login`, `addressenregister` and `jobs-controller`. [DL-6490]
 - Reorganize delta consumers config to harmonize with the ecosystem
+- Backend refactoring [CLBV-980]
 
 ### Deploy Notes
 ```
-drc up -d migrations login addressenregister jobs-controller op-consumer
+drc down
+cp -r data copy-data-2025-xx-xx
+rm -rf data
+drc up -d virtuoso migrations
+# wait for end of migrations
+drc up -d database op-consumer
+# wait for end of initial sync of the op consumer
+drc up -d
 ```
--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,6 +1,59 @@
 export default [
     {
       match: {
+        // anything
+      },
+      callback: {
+        url: "http://resource/.mu/delta", method: "POST"
+      },
+      options: {
+        resourceFormat: "v0.0.1",
+        gracePeriod: 1000,
+        ignoreFromSelf: true,
+        optOutMuScopeIds: [ "http://redpencil.data.gift/id/concept/muScope/deltas/consumer/initialSync" ]
+      }
+    },
+    {
+      match: {
+        predicate: {
+          type: 'uri',
+          value: 'http://www.w3.org/ns/adms#status',
+        },
+        object: {
+          type: 'uri',
+          value: 'http://redpencil.data.gift/id/concept/JobStatus/success',
+        },
+      },
+      callback: {
+        url: 'http://contact-data-dispatcher/initial-dispatch',
+        method: 'POST'
+      },
+      options: {
+        resourceFormat: "v0.0.1",
+        gracePeriod: 10000,
+        ignoreFromSelf: true
+      }
+    },
+    {
+      match: {
+        graph: {
+          type: 'uri',
+          value: 'http://mu.semte.ch/graphs/ingest'
+        }
+      },
+      callback: {
+        url: 'http://contact-data-dispatcher/delta',
+        method: 'POST'
+      },
+      options: {
+        resourceFormat: "v0.0.1",
+        gracePeriod: 10000,
+        ignoreFromSelf: true
+      }
+    }
+  /* Commenting out delta producer rules for now as the producers are not up yet.
+    {
+      match: {
         predicate: {
           type: 'uri',
           value: 'http://www.w3.org/ns/adms#status'
@@ -51,5 +104,5 @@ export default [
         ignoreFromSelf: true
       }
     }
-    // Other delta listeners
+    */
   ];
